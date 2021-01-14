810d63814c984cb3d10cf17af6f8df5890456a47
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-14 23:42:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,10 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
+  new_string = ""
   if string[0] == "s"  
     10.times 
-    "s" + string 
+    "s" + string = new_string
   else
   string 
 end
+new_string
 end
