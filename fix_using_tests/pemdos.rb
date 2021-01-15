ad585eae811d8027f34991e8f4fd4d2aaf1c8096
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-15 00:22:54 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,11 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  new_string = "ssssssssss"
+  if string[0] == "s"  
+  new_string<< "surprise!"
   else
-    string
-  end
+  string 
+end
+new_string
 end
