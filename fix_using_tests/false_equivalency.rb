591a9456305bbd22d3318c6016ac6102815a8919
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-15 00:05:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -6,13 +6,13 @@ def get_user_input
 end
 
 def prompt_user
+  binding.pry 
   puts "What would you like to do?"
   puts "1.)Eat a hamburger."
   puts "2.)Eat a ham."
 end
 
 def selection(num)
-  binding.pry 
   if num = 1   
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2  
